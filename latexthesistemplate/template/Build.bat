24d0e0b3a2604d51e3ac21954a3c3643ae9ac25a
Niklas Peter <niklas.peter@informatik.hs-fulda.de>
2016-10-01 22:41:00 +0200

    Ported settings from the custom template based on the old template version to the current version.

---
+++ latexthesistemplate/template/Build.bat
@@ -0,0 +1,17 @@
+REM Author: Niklas Peter
+REM Execute this script in the directory containing the template's main file.
+
+SET fullname=LaTeXTemplate.tex
+SET basename=LaTeXTemplate
+
+pdflatex %fullname%
+biber %basename%
+makeglossaries %basename%
+REM pdflatex has to be executed twice, because the first invocation
+REM might e. g. change the page numbering based on the data generated
+REM by biber and makeglossaries. So the second execution has to
+REM update e. g. the TOC.
+pdflatex %fullname%
+pdflatex %fullname%
+
+pause
