540acd4227f60b2c4c920a15736856838b037540
Niklas Peter <niklas.peter@informatik.hs-fulda.de>
2017-08-17 23:05:15 +0200

    Changes from bachelor thesis: no rounded inlinecode, hyphenation list expanded, SamePage environment and more new commands

--- latexthesistemplate/template/Build.bat
+++ latexthesistemplate/template/Build.bat
@@ -7,10 +7,12 @@ SET basename=LaTeXTemplate
 pdflatex %fullname%
 biber %basename%
 makeglossaries %basename%
-REM pdflatex has to be executed twice, because the first invocation
+REM pdflatex has to be executed three times, because the first invocation
 REM might e. g. change the page numbering based on the data generated
 REM by biber and makeglossaries. So the second execution has to
-REM update e. g. the TOC.
+REM update e. g. the TOC. The third execution adapts the indentation 
+REM of the TOC.
+pdflatex %fullname%
 pdflatex %fullname%
 pdflatex %fullname%
 
